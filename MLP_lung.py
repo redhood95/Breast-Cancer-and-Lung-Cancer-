fcbb205be392ec16ca506c1033265dcbb91cb631
yash upadhyay <yashup1997@gmail.com>
2018-08-19 15:54:12 +0530

    added mlp algorithm

--- MLP_lung.py
+++ MLP_lung.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Aug 19 15:31:29 2018
+Created on Sun Aug 19 15:46:17 2018
 
 @author: Yash Upadhyay
 """
@@ -8,22 +8,20 @@ Created on Sun Aug 19 15:31:29 2018
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+lung = pd.read_csv('lung.txt', sep=',',header=0)
+lung=lung.replace('?',0)
+X_lung = lung.iloc[:,0:56].values
+y_lung = lung.iloc[:, -1].values
 
-breast = pd.read_csv('breast.txt',sep=',',header=0)
-
-breast = breast.replace('?',0)
-
-X_breast = breast.iloc[:,1:10 ].values
-y_breast = breast.iloc[:, -1].values
 
 from sklearn.cross_validation import train_test_split
-Xb_train, Xb_test, yb_train, yb_test = train_test_split(X_breast, y_breast, test_size = 0.25, random_state = 0)
+Xl_train, Xl_test, yl_train, yl_test = train_test_split(X_lung, y_lung, test_size = 0.25, random_state = 0)
+
 
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
-
-Xb_train = sc.fit_transform(Xb_train)
-Xb_test = sc.transform(Xb_test)
+Xl_train = sc.fit_transform(Xl_train)
+Xl_test = sc.transform(Xl_test)
 
 import keras
 from keras.models import Sequential
@@ -32,7 +30,7 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Adding the input layer and the first hidden layer
-classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 9))
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 56))
 
 # Adding the second hidden layer
 classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
@@ -48,14 +46,15 @@ classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'si
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 # Fitting the ANN to the Training set
-classifier.fit(Xb_train, yb_train, batch_size = 10, epochs = 100)
+classifier.fit(Xl_train, yl_train, batch_size = 10, epochs = 100)
 
 # Part 3 - Making predictions and evaluating the model
 
 # Predicting the Test set results
-yb_pred = classifier.predict(Xb_test)
-yb_pred = (yb_pred > 0.5)
+yl_pred = classifier.predict(Xl_test)
+yl_pred = (yl_pred > 0.5)
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(yb_test, yb_pred)
+cm = confusion_matrix(yl_test, yl_pred)
+
